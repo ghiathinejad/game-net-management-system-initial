602d67c4b471a34c41e558980f7d700c3cf25fa8
ghiathinejad <m.ghiathinejad@gmail.com>
2022-12-21 00:24:28 +0330

    #3 controller

--- app/Controller/WalletController.php
+++ app/Controller/WalletController.php
@@ -7,11 +7,33 @@ use App\Models\WalletTransaction;
 use Core\Exceptions\NotFoundException;
 use Core\Request;
 use Core\Response;
+use mysql_xdevapi\Exception;
 
 class WalletController
 {
+    /**
+     * @return Response
+     * @throws NotFoundException
+     * @throws \Core\Exceptions\InvalidInputException
+     */
     public function recharge(): Response
     {
-        // TODO: Implement
+
+        $id = Request::getParam('user_id');
+        $amount = Request::getParam('amount');
+
+        try {
+            $user = User::getById($id)->toArray();
+            $date = date('Y-m-d H:i:s');
+            WalletTransaction::create(['user_id' => $id, 'amount' => $amount, 'type' => 'credit', 'created_at' => $date]);
+
+            $userUpdate['wallet_amount'] = $user['wallet_amount'] + $amount;
+
+            return Response::make()->setBody(User::update($id, $userUpdate)->toArray());
+        }catch (Exception $e){
+            echo 123;
+            return Response::make()->setStatusCode(404);
+        }
+
     }
 }
